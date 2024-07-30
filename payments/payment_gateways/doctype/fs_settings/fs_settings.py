85cf1869371bb57c37f3ce861088e0e661446e8a
karanwilson <karanav@vivaldi.net>
2024-07-30 16:23:07 +0530

    feat: Phase-1 FS Integration

--- payments/payment_gateways/doctype/fs_settings/fs_settings.py
+++ payments/payment_gateways/doctype/fs_settings/fs_settings.py
@@ -7,19 +7,36 @@ from frappe.model.document import Document
 from frappe.utils import call_hook_method
 from payments.utils import create_payment_gateway
 
-from zeep import Client
+from zeep import Client #,Settings
 
 class FSSettings(Document):
 	supported_currencies = ["INR"]
 
-	def	init_client(self):
-		if self.fs_user:
-			pass
-	
+	def fapi_login(self):
+		auth = {
+			"strPID": self.fs_user,
+			"strPassword": self.fs_password
+		}
+		#frappe.throw(auth["strPassword"])
+		# create the Zeep SOAP client object
+		fs_client = Client("assets/payments/FS.wsdl")
+		#return self.fs_client.service.
+		res = fs_client.service.login(auth)
+		frappe.throw(res["Result"])
+
 	def	validate(self):
 		create_payment_gateway("FS")
 		call_hook_method("payment_gateway_enabled", gateway="FS")
+		if not self.flags.ignore_mandatory:
+			self.validate_fs_credentials()
 	
+	def	validate_fs_credentials(self):
+		self.fapi_login()
+		# login to SOAP Server
+		#res = self.fapi_login()
+		#if res["Result"] != "OK":
+		#	frappe.throw(res["Result"])
+
 	def	validate_transaction_currency(self, currency):
 		if currency not in self.supported_currencies:
 			frappe.throw(
