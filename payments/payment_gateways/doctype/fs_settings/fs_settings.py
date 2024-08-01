46ff3901fd520b5bc8af908bda71194a70766898
karanwilson <karanav@vivaldi.net>
2024-08-01 12:57:30 +0530

    chore: Phase-3 FS Integration

--- payments/payment_gateways/doctype/fs_settings/fs_settings.py
+++ payments/payment_gateways/doctype/fs_settings/fs_settings.py
@@ -8,6 +8,10 @@ from frappe.utils import call_hook_method
 from payments.utils import create_payment_gateway
 
 from zeep import Client #, Settings
+from base64 import b64decode
+from oauthlib.common import urldecode
+from Crypto.Cipher import AES
+from datetime import datetime
 
 class FSSettings(Document):
 	supported_currencies = ["INR"]
@@ -20,18 +24,48 @@ class FSSettings(Document):
 	
 	def	validate_fs_credentials(self):
 		# login to SOAP Server
-		res = self.fapi_login()
-		self.login_status = res["Result"]
-		if self.login_status == "OK":
-			#frappe.throw(res["Result"])
-			pass
+		self.fapi_login()
+		self.request_transfer_token()
 
 	def fapi_login(self):
-		# create the Zeep SOAP client object
+		# Initialising the SOAP client
+		global fs_client
 		strPID = self.fs_user
 		strPassword = self.get_password(fieldname="fs_password", raise_exception=False)
 		fs_client = Client("assets/payments/FS.wsdl")
-		return fs_client.service.login(strPID, strPassword)
+
+		login_res = fs_client.service.login(strPID, strPassword)
+		self.login_status = login_res["Result"]
+		if self.login_status == "OK":
+			self.last_login_at = datetime.now()
+		
+		return login_res
+
+	def request_transfer_token(self):
+		param = [{"Req":""}]
+		fs_client = Client("assets/payments/FS.wsdl")
+		
+		token_res = fs_client.service.requestTransferToken(param)
+		self.token_response = token_res
+
+		encrypted = urldecode(token_res)
+		self.token_encrypted = encrypted[0][0]
+
+		d = encrypted[0][0].split(";")
+		self.encrypt_data = d[0]
+		self.encrypt_iv = d[1]
+
+		key = "fstockencryptkey".encode("utf8")
+		data = b64decode(d[0])
+		iv = b64decode(d[1])
+		
+		cipher = AES.new(key, AES.MODE_CBC, iv)
+		self.token_decrypted = cipher.decrypt(data)
+
+		#with open('fapi_tolen2.txt', 'w') as fp:
+		#	fp.write('\n'.join('%s %s' % x for x in encrypted))
+		#with open('tuple_file3.txt', 'w') as file:
+		#	file.write(str(encrypted[0][0]))
 
 	def	validate_transaction_currency(self, currency):
 		if currency not in self.supported_currencies:
