e007d894c41b01be248ace17c184af1f93f44ae1
karanwilson <karanav@vivaldi.net>
2024-08-07 10:04:18 +0530

    feat: FS Payments Integration

--- payments/payment_gateways/doctype/fs_settings/fs_settings.py
+++ payments/payment_gateways/doctype/fs_settings/fs_settings.py
@@ -7,6 +7,7 @@ from frappe.model.document import Document
 from frappe.utils import call_hook_method
 from frappe.integrations.utils import create_request_log
 from payments.utils import create_payment_gateway
+import json
 
 from zeep import Client #, Settings
 from base64 import b64decode
@@ -16,36 +17,39 @@ from datetime import datetime
 
 class FSSettings(Document):
 	supported_currencies = ["INR"]
+	# Initialise the SOAP client
+	fs_client = Client("assets/payments/FS.wsdl")
 
 	def	validate(self):
 		create_payment_gateway("FS")
 		call_hook_method("payment_gateway_enabled", gateway="FS")
 		if not self.flags.ignore_mandatory:
 			self.validate_fs_credentials()
-	
+
 	def	validate_fs_credentials(self):
 		# login to SOAP Server
 		login_res = self.fapi_login()
 		self.login_status = login_res["Result"]
 		if self.login_status == "OK":
-			self.last_login_at = datetime.now()
-		self.request_transfer_token()
+			self.login_at = datetime.now()
+		#self.request_transfer_token()
 
-	#@frappe.whitelist(allow_guest=True)
+
 	def fapi_login(self):
-		# Initialise the SOAP client
-		global fs_client
-		fs_client = Client("assets/payments/FS.wsdl")
-
 		strPID = self.fs_user
 		strPassword = self.get_password(fieldname="fs_password", raise_exception=False)
 
-		login_res = fs_client.service.login(strPID, strPassword)
+		login_res = self.fs_client.service.login(strPID, strPassword)
 		return login_res
+
+	def fapi_logout(self):
+		request = ""
+		return self.fs_client.service.logout(request)
+
 
 	def request_transfer_token(self):
 		request = {"transferToken":""}
-		token_res = fs_client.service.requestTransferToken(request)
+		token_res = self.fs_client.service.requestTransferToken(request)
 		encrypted = urldecode(token_res)
 		d = encrypted[0][0].split(";")
 		key = "fstockencryptkey".encode("utf8")
@@ -81,46 +85,87 @@ class FSSettings(Document):
 
 
 @frappe.whitelist(allow_guest=True)
-def add_transfer_contribution(doc, method):
+def login():
+	fs_controller = frappe.get_doc("FS Settings")
+	login_res = fs_controller.fapi_login()
+	return login_res["Result"]
 
-	def add_transfer(integration_request=None):
+@frappe.whitelist(allow_guest=True)
+def logout():
+	fs_controller = frappe.get_doc("FS Settings")
+	return fs_controller.fapi_logout()
+
+
+@frappe.whitelist(allow_guest=True)
+def add_transfer_contribution(doc, method):
+	# check if the Payment Entry is for PTDC Contibutions - the function doesn't run in case of returns and other Payment Entries
+	# the function also doesn't run in case a FS Transfer Status has value "OK"
+	if doc.custom_contribution_type:
+
 		fs_controller = frappe.get_doc("FS Settings")
-		# Add a login timeout check here, if possible
 		login_res = fs_controller.fapi_login()
 
 		if login_res["Result"] == "OK":
 			transfer_token = fs_controller.request_transfer_token()
+
 			if (transfer_token):
-				strAccountNumberFrom = frappe.get_value("Customer", doc.party, "custom_fs_account_number")
-				strAccountNumberTo = doc.custom_fs_account_to
-				fAmount = str(doc.paid_amount)
-				check = "Yes"
-				#with open('fapi_token8.txt', 'w') as file:
-				#	file.write(str(strAccountNumberFrom, strAccountNumberTo, fAmount, strDescription, check, transfer_token))
+				strAccountNumberFrom = frappe.get_value(doc.party_type, doc.party, "custom_fs_account_number")
+
+				payment_dict = {
+					'reference_doctype': doc.party_type,
+					'reference_docname': doc.party,
+					"Payment Name": doc.doctype,
+					"Payment ID": doc.name,
+					"strAccountNumberFrom": strAccountNumberFrom,
+					"strAccountNumberTo": fs_controller.fs_account,
+					"fAmount": str(doc.paid_amount),
+					# String format example: PTDC/EXTRA.CON/PAY-2024-00859/CLSQ524OS7
+					# string[0:5] extracts the first 4 chars of the string
+					"strDescription": _("PTDC/{0}.CON/{1}").format((doc.custom_contribution_type)[0:5], (doc.name)[4:]),
+					"check": "Yes",
+					"token": transfer_token
+				}
+
+				#with open('fapi.txt', 'w') as file:
+				#	file.write(str())
+
+				integration_request = None
+
+				# if exists, fetch the existing integration request for this "Payment Entry" doc
+				for integration_request_existing in frappe.get_all(
+					"Integration Request",
+					filters={"status": "Queued", "integration_request_service": "FS", },
+					fields=["name", "data"],
+				):
+					data = json.loads(integration_request_existing.data)
+					if data["Payment ID"] == doc.name :
+						integration_request = frappe.get_doc("Integration Request", integration_request_existing)
+						#payment_dict_json = frappe.as_json(payment_dict, indent=1)
+						#frappe.db.set_value("Integration Request", integration_request_existing.name, "data", payment_dict_json)
+						break
 
 				# Create an "Integration Request" in case of a fresh transfer
 				if not integration_request:
-					kwargs = {
-						"Payment Entry ID": doc.name,
-						"strAccountNumberFrom": strAccountNumberFrom,
-						"strAccountNumberTo": strAccountNumberTo,
-						"fAmount": fAmount,
-						# String format example: EXTRA.CON/ACC-PAY-2024-00857/7OHL1V6ATI
-						# string[0:5] extracts the first 4 chars of the string
-						"strDescription": _("PTDC/{0}.CON/{1}").format((doc.custom_contribution_type)[0:5], (doc.name)[4:]),
-						"check": check,
-						"token": transfer_token
-					}
 					# Create integration log
-					integration_request = create_request_log(kwargs, service_name="FS")
+					integration_request = create_request_log(payment_dict, service_name="FS")
+
 				# appending the integration_request name field as Transaction ID in strDescription
-				strDescription = _("{0}/{1}").format(kwargs["strDescription"], integration_request.name)
-				addTransfer_res = fs_client.service.addTransfer(
-					strAccountNumberFrom, strAccountNumberTo, fAmount, strDescription, check, transfer_token)
+				payment_dict["strDescription"] = _("{0}/{1}").format(payment_dict["strDescription"], integration_request.name)
+
+				addTransfer_res = fs_controller.fs_client.service.addTransfer(
+					payment_dict["strAccountNumberFrom"],
+					payment_dict["strAccountNumberTo"],
+					payment_dict["fAmount"],
+					payment_dict["strDescription"],
+					payment_dict["check"],
+					payment_dict["token"]
+				)
+
 				# Explore whether to store the default FS transaction message, or request for a transaction ID..
 				doc.custom_remarks = 1
 				doc.custom_fs_transfer_status = addTransfer_res["Result"]
 				doc.remarks = addTransfer_res["Message"]
+
 				if addTransfer_res["Result"] == "OK":
 					integration_request.status = "Completed"
 					integration_request.save(ignore_permissions=True)
@@ -130,25 +175,97 @@ def add_transfer_contribution(doc, method):
 					integration_request.save(ignore_permissions=True)
 					frappe.db.commit()
 					frappe.throw(addTransfer_res["Result"])
+
+			else:
+				frappe.throw("FS transfer token not received")
 		else:
 			frappe.throw(login_res["Result"])
 
-	if doc.custom_contribution_type and doc.custom_fs_transfer_status and doc.custom_fs_transfer_status != "OK":
-		# fetch the existing integration request with status "Queued" - for this "Payment Entry" doc
-		integration_request_existing = frappe.get_all(
-			"Integration Request",
-			filters={"integration_request_service": "FS", "data['Payment Entry ID']": doc.name},
-			fields=["name", "data"],
-		)
-		# Add logic to pass an existing "Integration Request", if any
-		if integration_request_existing:
-			integration_request = frappe.get_doc(integration_request_existing[0])
-			add_transfer(integration_request)
-
-	elif doc.custom_contribution_type and not doc.custom_fs_transfer_status:
-		add_transfer()
-
 
 @frappe.whitelist(allow_guest=True)
-def add_transfer_billing():
-	pass
+def add_transfer_billing(invoice_doc, fAmount):
+	invoice_dict = json.loads(invoice_doc)
+	#frappe.throw(invoice_dict["custom_fs_transfer_status"])
+
+	fs_controller = frappe.get_doc("FS Settings")
+	login_res = fs_controller.fapi_login()
+
+	if login_res["Result"] == "OK":
+		transfer_token = fs_controller.request_transfer_token()
+
+		if (transfer_token):
+			strAccountNumberFrom = frappe.get_value("Customer", invoice_dict["customer"], "custom_fs_account_number")
+
+			payment_dict = {
+				'reference_doctype': "Customer",
+				'reference_docname': invoice_dict["customer"],
+				"Payment Name": invoice_dict["doctype"],
+				"Payment ID": invoice_dict["name"],
+				"strAccountNumberFrom": strAccountNumberFrom,
+				"strAccountNumberTo": fs_controller.fs_account,
+				"fAmount": str(fAmount),
+				# String format example: PTDC/EXTRA.CON/PAY-2024-00859/CLSQ524OS7
+				# string[0:5] extracts the first 4 chars of the string
+				"strDescription": _("PT-POS-Invoice/{0}").format(invoice_dict["name"]),
+				"check": "Yes",
+				"token": transfer_token
+			}
+
+			#with open('fapi.txt', 'w') as file:
+			#	file.write(str())
+
+			integration_request = None
+
+			# if exists, fetch the existing integration request for this "Payment Entry" doc
+			for integration_request_existing in frappe.get_all(
+				"Integration Request",
+				filters={"status": "Queued", "integration_request_service": "FS", },
+				fields=["name", "data"],
+			):
+				data = json.loads(integration_request_existing.data)
+				if data["Payment ID"] == invoice_dict["name"] :
+					integration_request = frappe.get_doc("Integration Request", integration_request_existing)
+					#payment_dict_json = frappe.as_json(payment_dict, indent=1)
+					#frappe.db.set_value("Integration Request", integration_request_existing.name, "data", payment_dict_json)
+					break
+
+			# Create an "Integration Request" in case of a fresh transfer
+			if not integration_request:
+				# Create integration log
+				integration_request = create_request_log(payment_dict, service_name="FS")
+
+			# appending the integration_request name field as Transaction ID in strDescription
+			payment_dict["strDescription"] = _("{0}/{1}").format(payment_dict["strDescription"], integration_request.name)
+
+			addTransfer_res = fs_controller.fs_client.service.addTransfer(
+				payment_dict["strAccountNumberFrom"],
+				payment_dict["strAccountNumberTo"],
+				payment_dict["fAmount"],
+				payment_dict["strDescription"],
+				payment_dict["check"],
+				payment_dict["token"]
+			)
+
+			# Explore whether to store the default FS transaction message, or request for a transaction ID..
+			response = {
+				"custom_fs_transfer_status": addTransfer_res["Result"],
+				"remarks": addTransfer_res["Message"]
+			}
+
+			if addTransfer_res["Result"] == "OK":
+				integration_request.status = "Completed"
+				integration_request.save(ignore_permissions=True)
+				frappe.db.commit()
+				return response
+
+			else:
+				integration_request.staus = "Failed"
+				integration_request.save(ignore_permissions=True)
+				frappe.db.commit()
+				return response
+
+		else:
+			frappe.throw("FS transfer token not received")
+
+	else:
+			frappe.throw(login_res["Result"])
