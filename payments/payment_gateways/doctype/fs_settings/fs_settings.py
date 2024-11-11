eb351a064a74331a1db096dbbb57149d921d0cd0
Karan Wilson <karan.wilson@auroville.org.in>
2024-11-11 17:35:15 +0530

    CI: login date

--- payments/payment_gateways/doctype/fs_settings/fs_settings.py
+++ payments/payment_gateways/doctype/fs_settings/fs_settings.py
@@ -16,8 +16,8 @@ from zeep import Client #, Settings
 from base64 import b64decode
 from oauthlib.common import urldecode
 from Crypto.Cipher import AES
-import datetime
-from datetime import date
+#import datetime
+from datetime import datetime
 
 
 class FSSettings(Document):
